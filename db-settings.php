003530df2bce4b0be0d1a51e7f22b4d343873a45
Pranami Jhawar <jhawar.pranami@gmail.com>
2018-03-19 00:27:00 -0400

    heroku deployment

--- db-settings.php
+++ db-settings.php
@@ -7,11 +7,18 @@
  */
 //Development Database Information
 //$db_host = "10.0.0.105"; //Host address (most likely localhost)
-$db_host = "localhost";
-$db_name = "headphonesdb"; //Name of Database
-$db_user = "root"; //Name of database user
-$db_pass = "root"; //Password for database user
-$db_table_prefix = ""; // if the table prefix exists use this variable as a global
+//$db_host = "localhost";
+//$db_name = "headphonesdb"; //Name of Database
+//$db_user = "root"; //Name of database user
+//$db_pass = "root"; //Password for database user
+//$db_table_prefix = ""; // if the table prefix exists use this variable as a global
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$db_host = $url["host"];
+$db_user = $url["user"];
+$db_pass = $url["pass"];
+$db_name = substr($url["path"],1);
+
 
 
 //following variable declaration is for next class :)
